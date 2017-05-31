c854a628be720adc495bf1be97c007a339336f48
James Rossiter <j-ross1@live.com.au>
2017-05-31 22:12:58 +1000

    Project Files

---
+++ PerlinNoiseTerrainGen/Assets/Editor/MapEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*Creates a editor window in the Unity Inspector for the Noise Map Generator Script*/
+
+[CustomEditor(typeof (NoiseMapGenerator))] 
+public class MapEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        NoiseMapGenerator mapGen = (NoiseMapGenerator)target;
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.DrawMapInEditor();
+            }
+        }
+
+        if (GUILayout.Button("Generate Map"))
+        {
+            mapGen.DrawMapInEditor();
+        }
+    }
+}
---
+++ PerlinNoiseTerrainGen/Assets/Scripts/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    float speed = 100.0f;
+    float sprint = 250.0f;
+    float maxSpeed = 1000.0f;
+    float sensitivity = 0.25f;
+    private Vector3 previousMousePosition = new Vector3(255, 255, 255);
+    private float totalRun = 1.0f;
+
+    void Update()
+    {
+        previousMousePosition = Input.mousePosition - previousMousePosition;
+        previousMousePosition = new Vector3(-previousMousePosition.y * sensitivity, previousMousePosition.x * sensitivity, 0);
+        previousMousePosition = new Vector3(transform.eulerAngles.x + previousMousePosition.x, transform.eulerAngles.y + previousMousePosition.y, 0);
+        transform.eulerAngles = previousMousePosition;
+        previousMousePosition = Input.mousePosition;
+
+        Vector3 input = GetInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            input = input * totalRun * sprint;
+            input.x = Mathf.Clamp(input.x, -maxSpeed, maxSpeed);
+            input.y = Mathf.Clamp(input.y, -maxSpeed, maxSpeed);
+            input.z = Mathf.Clamp(input.z, -maxSpeed, maxSpeed);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1, 1000);
+            input *= speed;
+        }
+
+        input *= Time.deltaTime;
+        transform.Translate(input);
+
+    }
+
+    Vector3 GetInput()
+    {
+
+        Vector3 p_Velocity = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity.z += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity.z -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity.x += 1;
+        }
+        return p_Velocity;
+    }
+
+
+    
+}
